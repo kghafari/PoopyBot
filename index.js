fa435b151421318b992feb0a0a0d92e247cbe00d
kyle <kyleghafari@gmail.com>
2020-07-22 12:23:37 -0400

    father forgive me for i have sinned

---
+++ index.js
@@ -0,0 +1,45 @@
+const { Client, MessageEmbed } = require('discord.js');
+const config = require('./config.json');
+//const bot = new Discord.Client();
+
+// Create an instance of a Discord client
+const client = new Client();
+const token = config.token;
+const msg = "poopy";
+const stinky = "uh oh";
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+  // If the message is "how to embed"
+
+  if (message.content.toLowerCase().includes(msg)) {
+    message.react('💩');
+  }
+  if (message.content.toLowerCase().includes(stinky)) {
+    message.channel.send("stinky");
+  }
+});
+
+client.login(token);
+// Log our bot in using the token
+
+
+// const expression = "\bpoopy\b";
+
+// bot.on('ready', () => {
+//     console.log('This bot is online!');
+// })
+
+// bot.on('message', msg => {
+//     if(msg.content == "poopy"){
+//         msg.reply('haha funny');
+//     }
+// })
+
+// bot.login(token);
---
+++ readme.md
@@ -0,0 +1,9 @@
+# PoopyBot
+The worlds dumbest Discord bot. Please forgive me.
+
+https://www.youtube.com/watch?v=FveF-we6lcE
+
+# Instructions
+- run 'node .' to host locally
+- if you want to add this bot to your own sever, first off, god, why? Secondly... good luck with that.
+- regret everything
